3426a835b3dc6893f5003283ed59e8ecb1aaced3
leoatliang <luguoliang@yahoo.com>
2024-08-17 17:33:55 +0800

    updata

---
+++ cmake-build-release/CMakeFiles/OpencvTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpencvTest.dir/main.cpp.obj"
+  "CMakeFiles/OpencvTest.dir/main.cpp.obj.d"
+  "OpencvTest.exe"
+  "OpencvTest.exe.manifest"
+  "OpencvTest.pdb"
+  "libOpencvTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpencvTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
